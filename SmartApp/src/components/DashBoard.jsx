104377e9e465adbf3a5f8350f4a4b2826b745722
MiguelReprogramado <136768405+Miguelprogramer@users.noreply.github.com>
2025-09-01 19:55:09 -0300

    version 1.0.4

--- SmartApp/src/components/DashBoard.jsx
+++ SmartApp/src/components/DashBoard.jsx
@@ -1,5 +1,10 @@
 import "./DashBoard.css";
 import userImage from "../assets/user.png";
+import tvImage from "../assets/tv.png";
+import camaImage from "../assets/cama.png";
+import geladeiraImage from "../assets/geladeira.png";
+import chuveiroImage from "../assets/chuveiro.png";
+import lavanderiaImage from "../assets/lavanderia.png";
 
 function DashBoard() {
   return (
@@ -12,28 +17,24 @@ function DashBoard() {
         <div className="comodos">
           <p>Comodos:</p>
           <button className="comodo sala">
-            <img src="../assets/tv.png" alt="Sala" />
+            <img src={tvImage} alt="Sala" />
             <p>Sala</p>
           </button>
           <button className="comodo quarto">
-            <img src="../assets/cama.png.png" alt="Quarto" />
+            <img src={camaImage} alt="Quarto" />
             <p>Quarto</p>
           </button>
           <button className="comodo cozinha">
-            <img src="/assets/geladeira.png" alt="Cozinha" />
+            <img src={geladeiraImage} alt="Cozinha" />
             <p>Cozinha</p>
           </button>
           <button className="comodo banheiro">
-            <img src="/assets/chuveiro.png" alt="Banheiro" />
+            <img src={chuveiroImage} alt="Banheiro" />
             <p>Banheiro</p>
           </button>
           <button className="comodo lavanderia">
-            <img src="/assets/lavanderia.png" alt="Lavanderia" />
+            <img src={lavanderiaImage} alt="Lavanderia" />
             <p>Lavanderia</p>
-          </button>
-          <button className="comodo adicionar">
-            <img src="/assets/add.png" alt="Adicionar" />
-            <p>+</p>
           </button>
         </div>
       </div>
